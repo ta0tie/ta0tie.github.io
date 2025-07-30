617e1cb05a1154c6a62ee46e097dd88a44c58581
ta0tie <2463258637@qq.com>
2025-07-30 18:53:43 +0800

    Site updated: 2025-07-30 18:53:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/29/hello-anzhiyu/","2025/07/29/home_top-peoplecanvas(人潮涌动)魔改/"];function toRandomPost(){
+var posts=["posts/undefined/","posts/undefined/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
