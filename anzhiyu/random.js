eb1db4d133dd43510adf7e3f6231b0676ce833ad
ta0tie <2463258637@qq.com>
2025-07-31 13:47:49 +0800

    Site updated: 2025-07-31 13:47:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/50923400.html","posts/d87f7e0c.html","posts/peoplecanvas.html"];function toRandomPost(){
+var posts=["posts/aurorabackground.html","posts/50923400.html","posts/d87f7e0c.html","posts/peoplecanvas.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
