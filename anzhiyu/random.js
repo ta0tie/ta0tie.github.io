113b6875f035e33622b955eba7f5d3a1c92384b4
ta0tie <2463258637@qq.com>
2025-08-16 16:09:21 +0800

    Site updated: 2025-08-16 16:09:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/aurorabackground.html","posts/d87f7e0c.html","posts/50923400.html","posts/peoplecanvas.html","posts/6153d92.html","posts/de52e8d6.html"];function toRandomPost(){
+var posts=["posts/d87f7e0c.html","posts/6153d92.html","posts/aurorabackground.html","posts/50923400.html","posts/de52e8d6.html","posts/peoplecanvas.html","posts/algebrabasic.html","posts/numbertheory.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
